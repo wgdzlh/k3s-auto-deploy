786a39ee6c2af6a1fbcaedfa91faac68df79871f
wgdzlh <13d1101@gmail.com>
2021-03-18 01:20:50 +0800

    rancher setup configs.

--- after_setup_vms.sh
+++ after_setup_vms.sh
@@ -1,6 +1,10 @@
 vagrant halt
 
+NAT_NETWORK="vnet1"
+
+VBoxManage natnetwork add --netname ${NAT_NETWORK} --network 10.3.7.0/24 --dhcp on --ipv6 off
+
 for value in k8s-node-10 k8s-node-11 k8s-node-12; do
-    VBoxManage modifyvm $value --audio none --macaddress1 auto --nic1 natnetwork --nat-network1 "vnet1"
-    VBoxManage startvm $value --type headless
+    VBoxManage modifyvm ${value} --audio none --macaddress1 auto --nic1 natnetwork --nat-network1 ${NAT_NETWORK}
+    VBoxManage startvm ${value} --type headless
 done
